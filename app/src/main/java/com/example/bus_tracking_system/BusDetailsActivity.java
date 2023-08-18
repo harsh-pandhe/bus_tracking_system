d75de5e46df81a889c39625deb6f115788449ef3
harsh-pandhe <harshpandhehome@gmail.com>
2023-08-18 23:30:55 +0530

    Added Files

---
+++ app/src/main/java/com/example/bus_tracking_system/BusDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.bus_tracking_system;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BusDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_details);
+    }
+}
